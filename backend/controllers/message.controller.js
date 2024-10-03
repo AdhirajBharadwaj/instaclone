536b81aa7599ba081338b78b184f346e50dfa27d
Adhiraj Bharadwaj <adhirajbharadwaj30@gmail.com>
2024-10-03 07:13:10 +0530

    making message controller

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,61 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+export const sendMessage = async (req, res) => {
+    try
+    {
+        const {senderId} = req.id;
+        const receiverId=req.params.id;
+        const {message} = req.body;
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] } 
+        });
+
+        if(!conversation)
+        {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId]
+            });
+        }
+
+        const newMessage = await Message.create({
+            senderId: senderId,
+            receiverId: receiverId,
+            message: message
+        });
+        if(newMessage)
+        {
+            conversation.messages.push(newMessage._id);
+            await Promise.all([
+                conversation.save(),
+                newMessage.save()
+            ]);
+
+            return res.status(200).json({
+                message: "Message sent successfully",
+                success: true,
+            });
+        }
+        else
+        {
+            return res.status(400).json({
+                message: "Failed to send message",
+                success: false,
+            });
+        }
+        
+
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
